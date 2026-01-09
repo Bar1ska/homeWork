e3e4634939ef82688b517130831c2b4974823603
Николай Широлапов <nikolaishirolapov@yandex.ru>
2026-01-09 16:04:46 +0300

    [main] дз за 15,10,25 (1)

--- homeWork.cpp
+++ homeWork.cpp
@@ -1,42 +1,44 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 #include <Windows.h>
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    const int months = 12;
-    std::vector<int> profit(months);
+    const int totalSize = 10;
+    const int partSize = 5;
+    int array;
+    int firstArray, secondArray;
 
-    std::cout << "¬ведите прибыль за каждый из 12 мес€цев:\n";
-    for (int i = 0; i < months; ++i) {
-        std::cout << "ћес€ц " << i + 1 << ": ";
-        std::cin >> profit[i];
+    std::srand(std::time(nullptr));
+
+    for (int i = 0; i < totalSize; ++i) {
+        array = std::rand() % 100;
     }
 
-    int startMonth, endMonth;
-    std::cout << "¬ведите начальный мес€ц диапазона (от 1 до 12): ";
-    std::cin >> startMonth;
-    std::cout << "¬ведите конечный мес€ц диапазона (от 1 до 12): ";
-    std::cin >> endMonth;
-
-    if (startMonth < 1 || endMonth > 12 || startMonth > endMonth) {
-        std::cout << "Ќекорректный диапазон диапазона.\n";
-        return 1;
+    for (int i = 0; i < partSize; ++i) {
+        firstArray = array;
+        secondArray = array;
     }
 
-    auto range_start = profit.begin() + startMonth - 1;
-    auto range_end = profit.begin() + endMonth;
+    std::cout << "»сходный массив: ";
+    for (int i = 0; i < totalSize; ++i) {
+        std::cout << array << " ";
+    }
+    std::cout << std::endl;
 
-    auto max_it = std::max_element(range_start, range_end);
-    auto min_it = std::min_element(range_start, range_end);
+    std::cout << "ѕервый массив: ";
+    for (int i = 0; i < partSize; ++i) {
+        std::cout << firstArray << " ";
+    }
+    std::cout << std::endl;
 
-    int max_month = std::distance(profit.begin(), max_it) + 1;
-    int min_month = std::distance(profit.begin(), min_it) + 1;
-
-    std::cout << "ћес€ц с максимальной прибылью: " << max_month << " (" << *max_it << ")\n";
-    std::cout << "ћес€ц с минимальной прибылью: " << min_month << " (" << *min_it << ")\n";
+    std::cout << "¬торой массив: ";
+    for (int i = 0; i < partSize; ++i) {
+        std::cout << secondArray << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
