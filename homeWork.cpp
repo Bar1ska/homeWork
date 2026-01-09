55c3593f6bdecc4e915ca9a645e50ca64ede95fd
Николай Широлапов <nikolaishirolapov@yandex.ru>
2026-01-09 16:46:25 +0300

    [main] дз за 22,10,25

--- homeWork.cpp
+++ homeWork.cpp
@@ -1,59 +1,103 @@
 #include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <Windows.h>
+using namespace std;
 
-int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    int size;
-    std::cout << "Введите размер массивов: ";
-    std::cin >> size;
+void drawFigure(int n, int type)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            bool paint = false;
 
-    // Генерация диапазона для элементов
-    int minVal, maxVal;
-    std::cout << "Введите минимальное значение диапазона: ";
-    std::cin >> minVal;
-    std::cout << "Введите максимальное значение диапазона: ";
-    std::cin >> maxVal;
+            switch (type)
+            {
+                // a) треугольник под главной диагональю
+            case 1:  
+                paint = (i >= j);                           
+                break;
+                // б) треугольник над главной диагональю
+            case 2:  
+                paint = (i <= j);                           
+                break;
+                // в) треугольник над побочной диагональю
+            case 3:  
+                paint = (i + j <= n - 1);                   
+                break;
+                // г) треугольник под побочной диагональю
+            case 4:  
+                paint = (i + j >= n - 1);                   
+                break;
+                // д) верхний + нижний треугольники (по двум диагоналям)
+            case 5:  
+                paint = ((i <= j && i + j <= n - 1) || (i >= j && i + j >= n - 1));     
+                break;
+                // е) левый + правый треугольники (по двум диагоналям)
+            case 6:  
+                paint = ((i <= j && i + j >= n - 1) || (i >= j && i + j <= n - 1));     
+                break;
+                // ж) верхний треугольник (между диагоналями)
+            case 7:  
+                paint = (i <= j && i + j <= n - 1);         
+                break;
+                // з) правый треугольник (между диагоналями)
+            case 8:  
+                paint = (i <= j && i + j >= n - 1);         
+                break;
+                // и) нижний треугольник (между диагоналями)
+            case 9:  
+                paint = (i >= j && i + j >= n - 1);         
+                break;
+                // к) левый треугольник (между диагоналями)
+            case 10: 
+                paint = (i >= j && i + j <= n - 1);         
+                break;
+            }
 
-    // Инициализация генератора случайных чисел
-    std::srand(std::time(nullptr));
-
-    std::vector<int> array1(size);
-    std::vector<int> array2(size);
-    std::vector<int> result(size);
-
-    // Генерация массивов
-    for (int i = 0; i < size; ++i) {
-        array1[i] = minVal + std::rand() % (maxVal - minVal + 1);
-        array2[i] = minVal + std::rand() % (maxVal - minVal + 1);
+            cout << (paint ? '*' : ' ');
+        }
+        cout << '\n';
     }
+}
 
-    // Поэлементная сумма
-    for (int i = 0; i < size; ++i) {
-        result[i] = array1[i] + array2[i];
-    }
+int main()
+{
+    setlocale(LC_ALL, "Russian");
 
-    // Вывод результатов
-    std::cout << "Массив 1: ";
-    for (const auto& elem : array1) {
-        std::cout << elem << " ";
-    }
-    std::cout << std::endl;
+    int n;
+    cout << "Введите размер квадрата (рекомендуется 10–20): ";
+    cin >> n;
+    if (n < 2) n = 2;
 
-    std::cout << "Массив 2: ";
-    for (const auto& elem : array2) {
-        std::cout << elem << " ";
-    }
-    std::cout << std::endl;
+    int choice;
+    do
+    {
+        cout << "\nМеню фигур (квадрат " << n << "x" << n << "):\n";
+        cout << " 1 - фигура а\n";
+        cout << " 2 - фигура б\n";
+        cout << " 3 - фигура в\n";
+        cout << " 4 - фигура г\n";
+        cout << " 5 - фигура д\n";
+        cout << " 6 - фигура е\n";
+        cout << " 7 - фигура ж\n";
+        cout << " 8 - фигура з\n";
+        cout << " 9 - фигура и\n";
+        cout << "10 - фигура к\n";
+        cout << " 0 - выход\n";
+        cout << "Ваш выбор: ";
+        cin >> choice;
 
-    std::cout << "Результат (элементная сумма): ";
-    for (const auto& elem : result) {
-        std::cout << elem << " ";
-    }
-    std::cout << std::endl;
+        if (choice >= 1 && choice <= 10)
+        {
+            cout << '\n';
+            drawFigure(n, choice);
+        }
+        else if (choice != 0)
+        {
+            cout << "Неверный пункт меню!\n";
+        }
+
+    } while (choice != 0);
 
     return 0;
 }
+
