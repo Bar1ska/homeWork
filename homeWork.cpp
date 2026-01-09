1463f3970f055e37523bf9a55ca406e1b31edba5
Николай Широлапов <nikolaishirolapov@yandex.ru>
2026-01-09 17:02:37 +0300

    [main] дз за 5,11,25 (2)

--- homeWork.cpp
+++ homeWork.cpp
@@ -3,21 +3,19 @@
 using namespace std;
 
 int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
     int count = 0;
 
     for (int num = 100; num <= 999; num++) {
         int digit1 = num / 100;        
         int digit2 = (num / 10) % 10;  
         int digit3 = num % 10;         
-
-        if (digit1 == digit2 || digit1 == digit3 || digit2 == digit3) {
+        
+        if (digit1 != digit2 && digit1 != digit3 && digit2 != digit3) {
             count++;
         }
     }
 
-    cout << "Количество чисел с двумя одинаковыми цифрами: " << count << endl;
+    cout << "Количество трёхзначных чисел с разными цифрами: " << count << endl;
 
     return 0;
 }
