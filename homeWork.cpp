bae78c1e97858a60149f72bf898d49aa6b298bab
Николай Широлапов <nikolaishirolapov@yandex.ru>
2026-01-09 16:52:43 +0300

    [main] дз за 29,10,25 (1)

--- homeWork.cpp
+++ homeWork.cpp
@@ -1,103 +1,69 @@
 #include <iostream>
+#include <limits>
+#include <string>
+#include <Windows.h>
 using namespace std;
 
-void drawFigure(int n, int type)
-{
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0; j < n; ++j)
-        {
-            bool paint = false;
-
-            switch (type)
-            {
-                // a) треугольник под главной диагональю
-            case 1:  
-                paint = (i >= j);                           
-                break;
-                // б) треугольник над главной диагональю
-            case 2:  
-                paint = (i <= j);                           
-                break;
-                // в) треугольник над побочной диагональю
-            case 3:  
-                paint = (i + j <= n - 1);                   
-                break;
-                // г) треугольник под побочной диагональю
-            case 4:  
-                paint = (i + j >= n - 1);                   
-                break;
-                // д) верхний + нижний треугольники (по двум диагоналям)
-            case 5:  
-                paint = ((i <= j && i + j <= n - 1) || (i >= j && i + j >= n - 1));     
-                break;
-                // е) левый + правый треугольники (по двум диагоналям)
-            case 6:  
-                paint = ((i <= j && i + j >= n - 1) || (i >= j && i + j <= n - 1));     
-                break;
-                // ж) верхний треугольник (между диагоналями)
-            case 7:  
-                paint = (i <= j && i + j <= n - 1);         
-                break;
-                // з) правый треугольник (между диагоналями)
-            case 8:  
-                paint = (i <= j && i + j >= n - 1);         
-                break;
-                // и) нижний треугольник (между диагоналями)
-            case 9:  
-                paint = (i >= j && i + j >= n - 1);         
-                break;
-                // к) левый треугольник (между диагоналями)
-            case 10: 
-                paint = (i >= j && i + j <= n - 1);         
-                break;
-            }
-
-            cout << (paint ? '*' : ' ');
-        }
-        cout << '\n';
-    }
-}
-
-int main()
-{
-    setlocale(LC_ALL, "Russian");
-
-    int n;
-    cout << "Введите размер квадрата (рекомендуется 10–20): ";
-    cin >> n;
-    if (n < 2) n = 2;
-
+void menu() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     int choice;
-    do
-    {
-        cout << "\nМеню фигур (квадрат " << n << "x" << n << "):\n";
-        cout << " 1 - фигура а\n";
-        cout << " 2 - фигура б\n";
-        cout << " 3 - фигура в\n";
-        cout << " 4 - фигура г\n";
-        cout << " 5 - фигура д\n";
-        cout << " 6 - фигура е\n";
-        cout << " 7 - фигура ж\n";
-        cout << " 8 - фигура з\n";
-        cout << " 9 - фигура и\n";
-        cout << "10 - фигура к\n";
-        cout << " 0 - выход\n";
-        cout << "Ваш выбор: ";
+    do {
+        cout << "\nМеню:\n";
+        cout << "1. Ввести число\n";
+        cout << "0. Выход\n";
+        cout << "Выберите действие: ";
         cin >> choice;
 
-        if (choice >= 1 && choice <= 10)
-        {
-            cout << '\n';
-            drawFigure(n, choice);
-        }
-        else if (choice != 0)
-        {
-            cout << "Неверный пункт меню!\n";
-        }
+        switch (choice) {
+        case 1: {
+            long long num;
+            cout << "Введите число: ";
+            cin >> num;
 
+            // Обработка отрицательных чисел
+            long long abs_num = (num < 0) ? -num : num;
+
+            int digitCount = 0;
+            int zeroCount = 0;
+            long long sumDigits = 0;
+            long long temp = abs_num;
+
+            if (temp == 0) {
+                digitCount = 1;
+                zeroCount = 1;
+                sumDigits = 0;
+            }
+            else {
+                while (temp > 0) {
+                    int digit = temp % 10;
+                    sumDigits += digit;
+                    if (digit == 0)
+                        zeroCount++;
+                    digitCount++;
+                    temp /= 10;
+                }
+            }
+
+            double average = (digitCount != 0) ? static_cast<double>(sumDigits) / digitCount : 0;
+
+            cout << "Количество цифр: " << digitCount << endl;
+            cout << "Сумма цифр: " << sumDigits << endl;
+            cout << "Среднее арифметическое: " << average << endl;
+            cout << "Количество нулей: " << zeroCount << endl;
+
+            break;
+        }
+        case 0:
+            cout << "Выход из программы.\n";
+            break;
+        default:
+            cout << "Некорректный выбор. Попробуйте снова.\n";
+        }
     } while (choice != 0);
-
-    return 0;
 }
 
+int main() {
+    menu();
+    return 0;
+}
