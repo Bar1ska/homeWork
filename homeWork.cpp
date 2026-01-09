0652ca42b9e96d4f02259af5c768de313f329f2e
Николай Широлапов <nikolaishirolapov@yandex.ru>
2026-01-09 16:09:04 +0300

    [main] дз за 15,10,25 (2)

--- homeWork.cpp
+++ homeWork.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cstdlib>
 #include <ctime>
 #include <Windows.h>
@@ -6,37 +7,51 @@
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    const int totalSize = 10;
-    const int partSize = 5;
-    int array;
-    int firstArray, secondArray;
+    int size;
+    std::cout << "Введите размер массивов: ";
+    std::cin >> size;
 
+    // Генерация диапазона для элементов
+    int minVal, maxVal;
+    std::cout << "Введите минимальное значение диапазона: ";
+    std::cin >> minVal;
+    std::cout << "Введите максимальное значение диапазона: ";
+    std::cin >> maxVal;
+
+    // Инициализация генератора случайных чисел
     std::srand(std::time(nullptr));
 
-    for (int i = 0; i < totalSize; ++i) {
-        array = std::rand() % 100;
+    std::vector<int> array1(size);
+    std::vector<int> array2(size);
+    std::vector<int> result(size);
+
+    // Генерация массивов
+    for (int i = 0; i < size; ++i) {
+        array1[i] = minVal + std::rand() % (maxVal - minVal + 1);
+        array2[i] = minVal + std::rand() % (maxVal - minVal + 1);
     }
 
-    for (int i = 0; i < partSize; ++i) {
-        firstArray = array;
-        secondArray = array;
+    // Поэлементная сумма
+    for (int i = 0; i < size; ++i) {
+        result[i] = array1[i] + array2[i];
     }
 
-    std::cout << "Исходный массив: ";
-    for (int i = 0; i < totalSize; ++i) {
-        std::cout << array << " ";
+    // Вывод результатов
+    std::cout << "Массив 1: ";
+    for (const auto& elem : array1) {
+        std::cout << elem << " ";
     }
     std::cout << std::endl;
 
-    std::cout << "Первый массив: ";
-    for (int i = 0; i < partSize; ++i) {
-        std::cout << firstArray << " ";
+    std::cout << "Массив 2: ";
+    for (const auto& elem : array2) {
+        std::cout << elem << " ";
     }
     std::cout << std::endl;
 
-    std::cout << "Второй массив: ";
-    for (int i = 0; i < partSize; ++i) {
-        std::cout << secondArray << " ";
+    std::cout << "Результат (элементная сумма): ";
+    for (const auto& elem : result) {
+        std::cout << elem << " ";
     }
     std::cout << std::endl;
 
